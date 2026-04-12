d8505b0bc146c252a2d9668037d4cdb9a6ae6a64
rashmitasaini15-prog <rashmitasaini15@gmail.com>
2026-04-12 23:57:41 +0530

    updated appointment schema

--- backend/models/Appointment.js
+++ backend/models/Appointment.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
 const appointmentSchema = new mongoose.Schema({
-  patientId: String,
-  doctorId: String,
-  date: String,
-  status: { 
-    type: String, 
-    default: "pending" 
+  patientId: {
+    type: String,
+    required: true
+  },
+  patientName: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    default: "pending"
   }
 });
 module.exports = mongoose.model("Appointment", appointmentSchema);
