717a84182a620cf1f85ec4a27f6499559aa985b3
rashmitasaini15-prog <rashmitasaini15@gmail.com>
2026-04-12 23:23:30 +0530

    fixed port issue

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,7 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("DB Connected"))
 .catch(err => console.log(err));
 app.get("/", (req, res) => {
-  res.send("Backend Running ");
+  res.send("Backend Running 🚀");
 });
-// const aiRoutes = require("./routes/aiRoutes");
-// app.use("/api/ai", aiRoutes);
-app.listen(5000, () => console.log("Server running on 5000"));
-console.log(process.env.MONGO_URI);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
