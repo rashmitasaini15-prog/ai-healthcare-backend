1283bbd65e37ab74368042a585ff412a9d56cfc5
rashmitasaini15-prog <rashmitasaini15@gmail.com>
2026-04-14 21:06:19 +0530

    reminder update

--- backend/models/Reminder.js
+++ backend/models/Reminder.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 const reminderSchema = new mongoose.Schema({
   patientId: String,
   medicine: String,
-  time: String
+  time: String,
+  appointmentId: String,  
+  date: {
+    type: String,
+    default: new Date().toLocaleDateString()
+  }
 });
 module.exports = mongoose.model("Reminder", reminderSchema);
--- backend/routes/prescriptionRoutes.js
+++ backend/routes/prescriptionRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Prescription = require("../models/Prescription");
+const Reminder = require("../models/Reminder"); 
 router.post("/add", async (req, res) => {
   try {
     const { patientId, doctorId, appointmentId, medicines } = req.body;
@@ -8,11 +9,20 @@ router.post("/add", async (req, res) => {
     const newData = new Prescription({
       patientId,
       doctorId,
-      appointmentId: appointmentId || "NOT_SENT", 
+      appointmentId: appointmentId || "NOT_SENT",
       medicines
     });
     await newData.save();
-    res.json({ message: "Saved " });
+    for (let med of medicines) {
+      const newReminder = new Reminder({
+        patientId,
+        medicine: med,
+        time: "09:00",
+        appointmentId
+      });
+      await newReminder.save();
+    }
+    res.json({ message: "Prescription + Reminder Saved " });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Error " });
